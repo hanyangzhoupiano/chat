f8cdf283528fffd4caa8055bc911ac220cbf782a
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-05-22 23:14:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,28 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', ws => {
     ws.send(JSON.stringify({
-        username: "Server",
+        username: "System",
         message: "Welcome to the chatroom!"
     }));
     ws.on('message', message => {
         try {
             let data = JSON.parse(message);
+            let commands = {
+                "say": function(message) {
+                    if (message) {
+                        ws.send(JSON.stringify({
+                            username: "System",
+                            message: message
+                        }));
+                    };
+                },
+                "random": function() {
+                    ws.send(JSON.stringify({
+                        username: "System",
+                        message: "Generated a random integer from 1 to 100: " + Math.floor(Math.random() * 99 + 1)
+                    }));
+                }
+            };
 
             if (data.username && data.message) {
                 wss.clients.forEach(client => {
@@ -26,6 +42,17 @@ wss.on('connection', ws => {
                         }));
                     };
                 });
+
+                if (data.message[0] == "/") {
+                    let args = data.message.split(" ");
+                    let commandName = args.shift().substring(1);
+        
+                    if (commands[commandName] && args.length >= commands[commandName].length) {
+                        let command = commands[commandName](...args);
+    
+                        command();
+                    };
+                };
             };
         } catch (error) {
             console.warn("Error: " + error);
